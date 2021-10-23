31f79ea86a3c339462425e1c1a62380ab0d7bdb3
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-10-23 19:39:25 -0300

    exercício concluido

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  n1 , n2 ;
+
+            Console.WriteLine("Bem vindo ao calculo de Maior.\n");
+
+            Console.Write("Digite o primeiro número:");
+            n1=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo número:");
+            n2= Convert.ToInt32(Console.ReadLine());
+
+            if(n1>n2)
+            {
+
+            Console.WriteLine($" \n{n1} é menor que {n2}");
+        }   Console.WriteLine ($"\n{n2} é maior que {n1}");
+            
+            
+                 
+            }
+        }
+    }
+
